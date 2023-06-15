95ac3af0ee1fe1b8e7c38ccfaed4e8cdd5ad8a7a
ATsarev01 <maxperov01@gmail.com>
2023-06-15 19:37:50 +0600

    done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = new int[4];
+        try {
+            System.out.println("тест");
+            nums[7] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("О, нет, у нас ошибка");
+        }
     }
 }
